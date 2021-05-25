abbf96b4d7d4568da629aafea1f1768b7f960a3c
RiaTimm <82507151+RiaTimm@users.noreply.github.com>
2021-05-25 08:25:37 +0200

    Code with comments .py

--- Content/Working on/Dice Score/Code/Ria_Dicescore code.py
+++ Content/Working on/Dice Score/Code/Ria_Dicescore code.py
@@ -2,18 +2,21 @@
 import numpy as np
 import cv2 as cv2
 
-#designing the dice coefficient pt.1
+#designing the dice coefficient
+#general idea: image --> type 'array' --> type 'bool'
+
+# one way of doing it: 1.
 #loading the images (true = ground truth image; pred = predicted segmentation by SVM)
 img1_true = cv2.imread('path to image') #reads image into numpy array (2D matrix)
-img1_pred = cv2.imread('path to image')
+img1_pred = cv2.imread('path to image') #reads image into numpy array (2D matrix)
 
 #convert image into type bool
 img1t = img1_true.astype(bool)
 img1p = img1_pred.astype(bool)
 
-#designing the dice coefficient pt.2
-img2_true = imread('path to image') #import image
-img2_pred = imread('path to image') #import image
+# other way of doing it: 2.
+img2_true = imread('path to image') #import image as img2_true
+img2_pred = imread('path to image') #import image as img2_pred
 
 #convert image into type bool
 img2t = np.asarray(img1_true).astype(np.bool) #converts image to array and then to bool
@@ -21,14 +24,14 @@ img2p = np.asarray(img1_pred).astype(np.bool) #converts image to array and then 
 
 
 #defining the variables 'intersection' and 'union'
-intersection = np.logical_and(img1t, img1p) #Compute the truth value of x1 AND x2 element-wise
-union = img1t.sum() + img1p.sum()
+intersection = np.logical_and(img1t, img1p) #compute the truth value of x1 AND x2 element-wise = sums all pixels where both gt and pred have the value 'true'
+union = img1t.sum() + img1p.sum() #compute the truth value of x1 OR x2 element-wise = sums all pixels where either gt or pred (or both) have the value 'true'
 
 #calculating the dice
-if intersection + union == 0:
+if intersection + union == 0: #because it is mathematically not allowed to divide by 0, which would happen if gt and pred don't intersect
     print('dice cannot be calculated - no intersection')
 else:
-    dice = (2*intersection)/(union + intersection)
+    dice = (2*intersection)/(union + intersection) #using the dice formula to calculate the dice IF gt and pred intersect
     print(dice)
 
 #reusable code
@@ -36,13 +39,15 @@ else:
 
 # compute dice score
 def dice_coefficient(imgt, imgp):  # t = ground truth, p = SVM prediction
-    assert imgt.dtype == np.bool
-    assert imgp.dtype == np.bool
-    intersection = np.logical_and(imgt, imgp)
-    union = imgt.sum() + imgp.sum()
+    assert imgt.dtype == np.bool #the images with type array are converted to type bool
+    assert imgp.dtype == np.bool #the images with type array are converted to type bool
+    intersection = np.logical_and(imgt, imgp) #compute the truth value of x1 AND x2 element-wise = sums all pixels where both gt and pred have the value 'true'
+    union = imgt.sum() + imgp.sum() #compute the truth value of x1 OR x2 element-wise = sums all pixels where either gt or pred (or both) have the value 'true'
     if intersection + union == 0:
-        return ('dice cannot be calculated - no intersection')
+        return ('dice cannot be calculated - no intersection') #because it is mathematically not allowed to divide by 0, which would happen if gt and pred don't intersect
     else:
-        dice = (2 * intersection) / (union + intersection)
-        return dice
+        dice = (2 * intersection) / (union + intersection) #using the dice formula to calculate the dice IF gt and pred intersect
+        return dice #print out dice
+
+
 
