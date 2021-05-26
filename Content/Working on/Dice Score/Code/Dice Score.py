99447ff554c151818e996bf73af60ddde7315461
RiaTimm <82507151+RiaTimm@users.noreply.github.com>
2021-05-26 16:50:05 +0200

    Dice Score.py ist die vollständige & beste Version des Dice Score

--- Content/Working on/Dice Score/Code/Dice Score.py
+++ Content/Working on/Dice Score/Code/Dice Score.py
@@ -6,17 +6,17 @@ img1 = cv2.imread('Synthetic Image 1.png')
 img1a = cv2.imread('Synthetic Image 1a.png')
 img2 = cv2.imread('Synthetic Image 2.png')
 
-#defining function with variables for segmented picture and ground truth
+#designing reusable function
 def dice_coefficient(imgt, imgp):  # t = ground truth, p = SVM prediction
-    assert imgt.dtype == np.bool
-    #image as array
-    assert imgp.dtype == np.bool
-    intersection = np.logical_and(imgt, imgp)
-    union = imgt.sum() + imgp.sum()
+    assert imgt.dtype == np.bool #the images with type array are converted to type bool
+    assert imgp.dtype == np.bool #the images with type array are converted to type bool
+    intersection = np.logical_and(imgt, imgp) #compute the truth value of x1 AND x2 element-wise = sums all pixels where both gt and pred have the value 'true'
+    union = imgt.sum() + imgp.sum() #compute the truth value of x1 OR x2 element-wise = sums all pixels where either gt or pred (or both) have the value 'true'
     if intersection + union == 0:
-        return("dice cannot be calculated - no intersection")
+        return ('dice cannot be calculated - no intersection') #because it is mathematically not allowed to divide by 0, which would happen if gt and pred don't intersect
     else:
-        dice = (2 * intersection) / (union + intersection)
-        return dice
+        dice = (2 * intersection) / (union + intersection) #using the dice formula to calculate the dice IF gt and pred intersect
+        return dice #print out dice
 
+#using function dice_coefficient to compute the dice score
 dice_coefficient(img1, img1)
