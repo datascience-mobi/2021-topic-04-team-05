ad57866d592f6f11161ab9fd966239ec07ea2208
RiaTimm <82507151+RiaTimm@users.noreply.github.com>
2021-05-27 10:07:57 +0200

    different ways to calculate dice score - none working

--- Content/Working on/Dice Score/Code/Dice Score.py
+++ Content/Working on/Dice Score/Code/Dice Score.py
@@ -1,3 +1,5 @@
+import cv2
+import matplotlib as matplotlib
 import numpy as np
 import cv2
 
@@ -5,6 +7,30 @@ import cv2
 img1 = cv2.imread('Synthetic Image 1.png')
 img1a = cv2.imread('Synthetic Image 1a.png')
 img2 = cv2.imread('Synthetic Image 2.png')
+
+
+intersection = np.logical_and(img1, img1)
+union = img1.sum() + img1.sum() #np.sum(img1() + img1())
+dice = (2 * intersection) / (union + intersection)
+print (dice)
+
+
+#reading in images
+img1 = cv2.imread('Synthetic Image 1.png') #as array
+img1a = cv2.imread('Synthetic Image 1a.png')
+img2 = cv2.imread('Synthetic Image 2.png')
+
+def dice_coefficient(imgt, imgp):
+    imgt = np.dtype(bool) #oder imgt.astype('bool')
+    imgp = np.dtype(bool)
+    intersection = np.logical_and(imgt, imgp) #compute the truth value of x1 AND x2 element-wise = sums all pixels where both gt and pred have the value 'true'
+    union = np.logical_or(imgt, imgp) #compute the truth value of x1 OR x2 element-wise = sums all pixels where either gt or pred (or both) have the value 'true'
+    dice = (2 * intersection.sum) / (union + intersection) #using the dice formula to calculate the dice IF gt and pred intersect
+    return dice #print out dice
+
+dice_coefficient(img1, img1)
+
+
 
 #designing reusable function
 def dice_coefficient(imgt, imgp):  # t = ground truth, p = SVM prediction
@@ -19,4 +45,18 @@ def dice_coefficient(imgt, imgp):  # t = ground truth, p = SVM prediction
         return dice #print out dice
 
 #using function dice_coefficient to compute the dice score
+dice_coefficient(img1, img1)
+
+
+def dice_coefficient(imgt, imgp):
+    imgt = np.dtype(np.bool)
+    imgp = np.dtype(np.bool)
+    intersection = np.logical_and(imgt, imgp) #compute the truth value of x1 AND x2 element-wise = sums all pixels where both gt and pred have the value 'true'
+    union = imgt.sum() + imgp.sum() #compute the truth value of x1 OR x2 element-wise = sums all pixels where either gt or pred (or both) have the value 'true'
+    if intersection + union == 0:
+        return ('dice cannot be calculated - no intersection') #because it is mathematically not allowed to divide by 0, which would happen if gt and pred don't intersect
+    else:
+        dice = (2 * intersection) / (union + intersection) #using the dice formula to calculate the dice IF gt and pred intersect
+        return dice #print out dice
+
 dice_coefficient(img1, img1)
