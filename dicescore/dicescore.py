d8cd85d658b36fee2490098378141c2110a08f5a
RiaTimm <82507151+RiaTimm@users.noreply.github.com>
2021-06-06 13:54:20 +0200

    meeting file

--- dicescore/dicescore.py
+++ dicescore/dicescore.py
@@ -5,13 +5,13 @@ from matplotlib.image import imread
 from scipy import misc, ndimage
 
 def dice_score (pred,gt):
-    preduint8 = pred.astype(np.uint8)
-    gtuint8 = gt.astype(np.uint8)
-    intersection = np.logical_and(preduint8, gtuint8)
-    dice = (2 * intersection.sum()) / (preduint8.sum() + gtuint8.sum())
+    preduint8 = pred.astype(np.uint8) #convert prediction image to different array type: uint8 = 8 bit unsigned integer (range of values: 0 to 255)
+    gtuint8 = gt.astype(np.uint8) #convert ground truth to array type uint8
+    intersection = np.logical_and(preduint8, gtuint8) #calculate the intersection of both images, the  truth value of x1 AND x2 element-wise
+    dice = (2 * intersection.sum()) / (preduint8.sum() + gtuint8.sum()) #calculate dice (2*intersection/union)
     print(dice)
 
-img1 = imread ('SyntheticImage1.png')
-img2 = imread ('SyntheticImage2.png')
+img1 = imread ('SyntheticImage1.png') #import image
+img2 = imread ('SyntheticImage2.png') #import image
 
 dice_score (img1, img2)
