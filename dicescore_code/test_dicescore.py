828360a183b9235cf6453569ac67917d305ce7dd
RiaTimm <82507151+RiaTimm@users.noreply.github.com>
2021-06-07 20:46:04 +0200

    unit test

--- dicescore_code/test_dicescore.py
+++ dicescore_code/test_dicescore.py
@@ -3,11 +3,14 @@ import numpy as np
 from matplotlib.image import imread
 
 from dicescore import dice_score
+from dicescore_code import dicescore
 
-img1 = np.asarray(imread('man_seg21testing.png'))
+img1 = np.asarray(imread('29test.png'))
 
 class TestDiceScore(unittest.TestCase):
     def test_dice_score(self):
-        result = dice_score(img1, img1)
+        result = dicescore.dice_score(img1, img1)
         self.assertEqual(result, 1.0) #checks if a (result) == b (1) (dice score should be 1 for image with itself)
 
+if __name__ == '__main__': #to use run instead of having to use the terminal
+    unittest.main()
