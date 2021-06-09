2adac8b211c938de7dac6f4c51df5469899bff51
RiaTimm <82507151+RiaTimm@users.noreply.github.com>
2021-06-09 12:31:47 +0200

    dice score

--- functions/dicescore_code/dicescore.py
+++ functions/dicescore_code/dicescore.py
@@ -1,17 +1,18 @@
-#DICE SCORE
-#python library for working with arrays
+# DICE SCORE
+# python library for working with arrays
 import numpy as np
-#support for opening, manipulating, and saving images
-import PIL
+# support for opening, manipulating, and saving images
 
-#reading the images and converting to arrays
+# reading the images and converting to arrays
 from matplotlib.image import imread
 
-img1 = np.asarray(imread('man_seg21testing.png'))
-#img1 = np.asarray(PIL.Image.open('man_seg21_totest.png'))
 
-def dice_score (pred, gt):
+def dice_score(pred, gt):
     dice = np.sum(pred[gt == pred]) * 2.0 / (np.sum(gt) + np.sum(pred))
-    print (dice)
+    print(dice)
 
-dice_score (img1, img1)
+
+if __name__ == '__main__':
+    img1 = np.asarray(imread('man_seg21testing.png'))
+    # img1 = np.asarray(PIL.Image.open('man_seg21_totest.png'))
+    dice_score(img1, img1)
--- functions/dicescore_code/test_dicescore.py
+++ functions/dicescore_code/test_dicescore.py
@@ -4,10 +4,11 @@ from matplotlib.image import imread
 
 from functions.dicescore_code import dicescore
 
-img1 = np.asarray(imread('29test.png'))
 
 class TestDiceScore(unittest.TestCase):
-    def test_dice_score(self):
-        result = dicescore.dice_score(img1, img1)
-        self.assertEqual(result, 1.0) #checks if a (result) == b (1) (dice score should be 1 for image with itself)
+    def setUp(self) -> None:
+        self.img = np.asarray(imread('29test.png'))
 
+    def test_dice_score(self):
+        result = dicescore.dice_score(self.img, self.img)
+        self.assertEqual(result, 1.0)  # checks if a (result) == b (1) (dice score should be 1 for image with itself)
