7fe81b9694f40aa7101f532d545e4fb801169756
MichelleEmmert <78653350+MichelleEmmert@users.noreply.github.com>
2021-06-16 11:06:37 +0200

    edited SVM code and renamed some Meeting files

--- SVM_Segmentation/PCA.py
+++ SVM_Segmentation/PCA.py
@@ -1,38 +1,54 @@
 import os
 import cv2
+import pandas as pd
 from numpy import asarray
 from sklearn.decomposition import PCA as RandomizedPCA
-from PIL import Image
+import sklearn
+import skimage
+from skimage import io
+
 
 #Reading images from folder as NumpyNDArray
 def load_images_from_folder(folder):
     image_list = []
     for filename in os.listdir(folder):
-        img = Image.open(os.path.join(folder, filename))
+        img = io.imread(os.path.join(folder, filename))
         if img is not None:
             array = asarray(img)
             image_list.append(array)
+    #image_list_dataframe = pd.DataFrame(image_list)
     return image_list
 
-listimg = load_images_from_folder("/Users/juanandre/PycharmProjects/svm/Data/N2DH-GOWT1/img")
-listgt = load_images_from_folder("/Users/juanandre/PycharmProjects/svm/Data/N2DH-GOWT1/gt/tif")
-
+listimg = load_images_from_folder("../../Data/N2DH-GOWT1/img")
+listgt = load_images_from_folder("../../Data/N2DH-GOWT1/gt/tif")
 
 #apply pca to images
 def convert_pca(image_list):
-    for image in image_list:
-        pca = RandomizedPCA(1000).fit(image.data)
+    pca_list = []
+    for image in listimg:
+        pca = RandomizedPCA(300).fit(image.data)
         components = pca.transform(image.data)
         projected = pca.inverse_transform(components)
         if projected is not None:
-            array = asarray(projected)
-            list_pca = image_list.append(array)
-    return list_pca
+            pca_list.append(projected)
+    return pca_list
 
 pca_listimg_trial = convert_pca(listimg)
 pca_listgt_trial = convert_pca(listgt)
 
-#print(pca_listimg_trial[1].shape)
+#pca image has 1024x1024 pixels -> gt can be used originally
+    #test = pca_listimg_trial[1]
+    #print(test)
+    #print(test.shape)
+
+# test if all images are included and went through pca and show first and last
+    #print(pca_listimg_trial)
+    #cv2.imshow('img1', listimg[0])
+    #cv2.imshow('img6', listimg[5])
+    #cv2.imshow('Trial1', pca_listimg_trial[0])
+    #cv2.imshow('Trial6', pca_listimg_trial[5])
+    #cv2.waitKey()
+
 
 #cv2.imshow("Bild", pca_listimg_trial)
 #cv2.waitKey()
