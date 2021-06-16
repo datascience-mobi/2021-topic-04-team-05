59efa721fdd9b9823587dbd03e9ffb2e4c1d92fe
MichelleEmmert <78653350+MichelleEmmert@users.noreply.github.com>
2021-06-16 13:13:23 +0200

    edited PCA

--- SVM_Segmentation/PCA.py
+++ SVM_Segmentation/PCA.py
@@ -4,6 +4,7 @@ import pandas as pd
 from numpy import asarray
 from sklearn.decomposition import PCA as RandomizedPCA
 from skimage import io
+import imagecodecs
 
 
 #Reading images from folder as NumpyNDArray
@@ -17,8 +18,8 @@ def load_images_from_folder(folder):
     #image_list_dataframe = pd.DataFrame(image_list)
     return image_list
 
-listimg = load_images_from_folder("../../Data/N2DH-GOWT1/img")
-listgt = load_images_from_folder("../../Data/N2DH-GOWT1/gt/tif")
+listimg = load_images_from_folder("../Data/N2DH-GOWT1/img")
+listgt = load_images_from_folder("../Data/N2DH-GOWT1/gt/tif")
 
 #apply pca to images
 def convert_pca(image_list):
@@ -41,11 +42,11 @@ pca_listgt_trial = convert_pca(listgt)
 
 # test if all images are included and went through pca and show first and last
     #print(pca_listimg_trial)
-    #cv2.imshow('img1', listimg[0])
+cv2.imshow('img1', listimg[0])
     #cv2.imshow('img6', listimg[5])
     #cv2.imshow('Trial1', pca_listimg_trial[0])
     #cv2.imshow('Trial6', pca_listimg_trial[5])
-    #cv2.waitKey()
+cv2.waitKey()
 
 
 #cv2.imshow("Bild", pca_listimg_trial)
