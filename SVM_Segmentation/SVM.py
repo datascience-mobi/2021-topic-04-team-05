ff2a3a3c1c304cb1dce4ed2fefdac3bd759e42b0
RiaTimm <82507151+RiaTimm@users.noreply.github.com>
2021-06-24 16:51:19 +0200

    svm

---
+++ SVM_Segmentation/SVM.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split as tts
+from sklearn.metrics import accuracy_score, recall_score, precision_score
+import random
+import cv2
