df7e2d24c27e96127c6b6a451a72df1d879100e6
RiaTimm <82507151+RiaTimm@users.noreply.github.com>
2021-06-24 16:52:49 +0200

    svm

--- SVM_Segmentation/SVM.py
+++ SVM_Segmentation/SVM.py
@@ -6,3 +6,17 @@ from sklearn.model_selection import train_test_split as tts
 from sklearn.metrics import accuracy_score, recall_score, precision_score
 import random
 import cv2
+
+C =
+learning_rate =
+
+def loss_function (x,w,y):
+    #calculate hinge loss
+    N = x.shape[0]  #number of rows in x = number of samples
+    separation = distance_of_point_to_hyperplane(w, x, y)  #calculates distance of x to hyperplane
+    separation = [0 if i < 0 else i for i in separation] #all negative seperation values are replaced by 0
+    hinge_loss = C * (np.sum(separation) / N)  # average loss because the whole Y is taken & it encompasses several samples
+
+    # calculate loss
+    loss = 1 / 2 * np.dot(w, w) + hinge_loss  # np.dot (W,W) ist gleich Betrag von w^2
+    return loss
