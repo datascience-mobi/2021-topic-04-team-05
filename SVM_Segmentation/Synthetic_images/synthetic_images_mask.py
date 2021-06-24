66e73d5845d99d7e23476545b535880728eb2c09
laurasanchis <lasanpla@gmail.com>
2021-06-24 20:50:01 +0200

    attempt to overwrite pixels in background image with the pixels from the foreground png.

--- SVM_Segmentation/Synthetic_images/synthetic_images_mask.py
+++ SVM_Segmentation/Synthetic_images/synthetic_images_mask.py
@@ -42,10 +42,38 @@ def foreground(path_of_folder_with_mask):
             print('Only png or jpg files can be used as background.')
     else:
         print('Wrong background image-path: {}'.format(path_of_background))
+        return  backgroundimg
 
-    return  composedimage
 
-compimage('Pictures/Foreground/1.png', 'Pictures/Background/1.jpg')
+def compimage(foregroundimg, backgroundimg):
+    # Rotate the foreground
+    angle = random.randint(0, 359)
+    foregroundrotated = sktrans.rotate(foregroundimg, angle)
+
+    # Scale the foreground
+    scale = random.uniform(0.1,1)
+    new_size = (int(foregroundimg.shape[0] * scale), int(foregroundimg.shape[1] * scale))
+    foregroundscaled = sktrans.resize(foregroundrotated, new_size)
+
+    # Choose a random x,y position for the foreground
+    max_pos = (backgroundimg.shape[0] - foregroundimg.shape[0], backgroundimg.shape[1] - foregroundimg.shape[1])
+    if max_pos[0] <= 0 or max_pos[1] <= 0:
+        raise (f'foreground is too big for the background.')
+    position = (random.randint(0, max_pos[0]), random.randint(0, max_pos[1]))
+
+    # Create a new foreground image as large as the background and paste it on top
+    foregroundmask = Image.new('RGBA', backgroundimg.shape, color=(0, 0, 0, 0))
+    foregroundnew =  foregroundmask.paste(foregroundscaled, position)
+
+    composedimage = np.zeros(backgroundimg.shape)
+    for i in foregroundnew.getchannel(3):
+        for j in backgroundimg:
+            for h in composedimage:
+                if i > 0:
+                    composedimage[h] += foregroundnew.getchannel(3)[i]
+                else:
+                    composedimage[h] += backgroundimg[j]
+    return composedimage
 
 #img = Image.open('Pictures/Background/1.jpg')
 #print(img)
