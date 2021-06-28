ee59c0c7675ebe83a32781c1604a3c426d4416af
MichelleEmmert <78653350+MichelleEmmert@users.noreply.github.com>
2021-06-28 15:16:56 +0200

    edited readimages.py, PCA.py and dicescore.py

--- functions/dicescore.py
+++ functions/dicescore.py
@@ -8,6 +8,12 @@ from matplotlib.image import imread
 
 
 def dice_score(pred, gt):
+    """
+    This function calculates the similiarity between two arrays.
+    :param pred: an array of predicted labels
+    :param gt: the ground truth of this array
+    :return: a value between 0 and 1, describing the similiarity between those arrays. 1 is the dice score of similiar arrays.
+    """
     dice = np.sum(pred[gt == pred]) * 2.0 / (np.sum(gt) + np.sum(pred))
     print(dice)
 
