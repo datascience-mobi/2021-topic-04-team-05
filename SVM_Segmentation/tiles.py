74a8b17761da5b8b94772c593566f53d0f59ea0f
laurasanchis <lasanpla@gmail.com>
2021-07-08 20:51:27 +0200

    svm test

--- SVM_Segmentation/tiles.py
+++ SVM_Segmentation/tiles.py
@@ -1,3 +1,20 @@
+import numpy as np
+import pandas as pd
+import math
+import statsmodels.api as sm
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split as tts
+from sklearn.metrics import accuracy_score, recall_score, precision_score
+from sklearn.utils import shuffle
+from skimage.transform import resize
+from skimage import io
+import sklearn.decomposition as skdecomp
+from sklearn.preprocessing import StandardScaler
+from matplotlib import pyplot as plt
+import cv2
+import array_to_img as ai
+
+
 def tiles(image, number):
     list = [] #creates an empty list, which will be filled during the iterations with the tiles
     M = image.shape[0]//number #M=total number of lines, divided through the number = # of tiles
@@ -8,4 +25,22 @@ def tiles(image, number):
         # and the amount of steps is the N which was already calculated
             list.append([image[x:x + M, y:y + N]]) #the already created, empty list, is appended with the tile which
         # is "cut" during each iteration
-    return list
+    list_mean = []
+    for i in range(0, len(list)):
+        mean = np.mean(list[i])
+        list_mean.append(mean)
+    return list_mean
+
+if __name__ == '__main__':
+    def oneD_array_to_twoD_array(oneDarray):
+        twoDarray = np.stack(oneDarray, axis=0)
+        a = int(math.sqrt(len(twoDarray)))
+        twoDarray = twoDarray.reshape(a, a)
+        return twoDarray
+
+    gtread = io.imread('../Data/test/gt/man_seg01.jpg')
+    gttiles = tiles(gtread, 50)
+    gtiles_array = np.asarray(gttiles)
+    gttwod = oneD_array_to_twoD_array(gtiles_array)
+    plt.imshow(gttwod)
+    plt.show()
