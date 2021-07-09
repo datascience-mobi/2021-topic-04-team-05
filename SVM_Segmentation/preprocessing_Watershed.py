9c3a40e00e608d12770c0f0c13dacc95ea0b6dbc
juanandre <juan.andre10@yahoo.com>
2021-07-09 14:17:11 +0200

    Watershed and Gauss

--- SVM_Segmentation/preprocessing_Watershed.py
+++ SVM_Segmentation/preprocessing_Watershed.py
@@ -1,66 +1,53 @@
-import cv2
-import readimages as rm
-from scipy import ndimage
 import matplotlib.pyplot as plt
-from skimage.color import rgb2gray
-from skimage.morphology import dilation
+import cv2
+import os
 import numpy as np
-from skimage.segmentation import watershed
-from skimage.feature import peak_local_max
 
+def watershed(path_to_folder):
+    images = []
+    for filename in os.listdir(path_to_folder):
+        original_image = cv2.imread(os.path.join(path_to_folder,filename))
+        # Segmentation through threshold
+        grayconverted = cv2.cvtColor(original_image, cv2.COLOR_RGB2GRAY)
+        ret, thresh = cv2.threshold(grayconverted, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        # Further noise removal from thresholded image, the kernel matrix in this case (4,4) -> 4x4
+        kernel = np.ones((4, 4), np.uint8)
+        opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+        # clear background area by matrix dilation
+        clear_background = cv2.dilate(opening, kernel, iterations=3)
+        # Finding clear foreground area
+        dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+        ret, clear_foreground = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
+        # Finding unknown region
+        clear_foreground = np.uint8(clear_foreground)
+        unknown = cv2.subtract(clear_background, clear_foreground)
+        # Marker labelling
+        ret, markers = cv2.connectedComponents(clear_foreground)
+        # Add one to all labels so that sure background is not 0, but 1
+        markers = markers + 1
+        # Now, mark the region of unknown with zero
+        markers[unknown == 255] = 0
+        markers = cv2.watershed(original_image, markers)
+        original_image[markers == -1] = [255, 0, 0]
 
-def watershed(path_of_image_folder):
-    image_list = rm.read_image(path_of_image_folder)
-    watershed_image_list = []
-    for image in watershed_image_list:
-        image1 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        gray = rgb2gray(image1)  # erstezen durch skimage/matplotlib funktion
-        ret1, thresholded = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)  # ersetzen durch skimage/matplotlib funktion
-        kernel = np.ones((3, 3), np.uint8)
-        dilated = dilation(thresholded, kernel, iterations=9)  # ersetzen durch skimage/matplotlib funktion
-    # the kernel matrix in this case (3,3) -> 3x3, and the iterations should be adapted in every image collection
-        distance_map = ndimage.distance_transform_edt(thresholded)
-    # Compute Euclidean distance from every binary pixel to the nearest zero pixel then find peaks
-        local_max = peak_local_max(distance_map, indices=False, min_distance=60, labels=dilated)
-    # Perform connected component analysis then apply Watershed
-        markers = ndimage.label(local_max, structure=np.ones((3, 3)))[0]
-        labels = watershed(-distance_map, markers, mask=dilated)
-    # Iterate through unique labels
-        total_area = 0
-        unique_elements = np.unique(labels)
-        for label in unique_elements:
-            if label == 0:
-                continue
-
-            # Create a mask
-        mask = np.zeros(gray.shape, dtype="uint8")
-        mask[labels == label] = 255
-
-        # Find contours
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-        cv2.drawContours(gray, [max(cnts)], -1, (36, 255, 12), 4)
-        watershed_image_list.append(image)
-
-    return image_list
+        if original_image is not None:
+            images.append(original_image)
+    return images
 
 if __name__ == '__main__':
-    for i in range(1,6):
-        a = watershed('/Users/juanandre/PycharmProjects/2021-topic-04-team-05/Data/N2DH-GOWT1/img')
-        plt.imshow(a[i])
+
+    path = ("/Users/juanandre/PycharmProjects/2021-topic-04-team-05/Data/N2DH-GOWT1/img")
+    max = os.listdir(path)
+    for i in range (1, len(max)):
+        segmented_images = watershed(path)
+        plt.imshow(segmented_images[i])
         plt.show()
 
 
 
-   # for i in range(1, 6):
-     #   img = watersheded_images
-     #   if img is not None:
-     #       img = float(img)
- #   plt.imshow(img)
-  #  plt.show()
 
-#cv2.imshow('Trial', watersheded_images)[0]
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
 
-#print(dtype(img))
+
+
+
+
