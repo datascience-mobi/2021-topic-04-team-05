e0cd433f5746117d77ece1051fcd755c3e5594b3
RiaTimm <82507151+RiaTimm@users.noreply.github.com>
2021-07-10 18:03:01 +0200

    comments tiles

--- SVM_Segmentation/preparation_tiles.py
+++ SVM_Segmentation/preparation_tiles.py
@@ -23,20 +23,18 @@ def tiles(image_path, number):
     names = rm.read_imagename(image_path)
     list_of_arrays = []
     for index, image in enumerate(images):
-        list = [] #creates an empty list, which will be filled during the iterations with the tiles
-        M = image.shape[0]//number #M=total number of lines, divided through the number = # of tiles
-        N = image.shape[1]//number #N=total number of columns, divided through the number = # of tiles
-        for x in range(0, image.shape[0], M): #iterations, starting in the first pixel until the last line,
-            # and the amount of steps is the M which was already calculated
-            for y in range(0, image.shape[1], N): #iterations, starting in the first pixel until the last column,
-            # and the amount of steps is the N which was already calculated
-                list.append([image[x:x + M, y:y + N]]) #the already created, empty list, is appended with the tile which
-        # is "cut" during each iteration
+        list = []
+        M = image.shape[0]//number
+        N = image.shape[1]//number
+        for x in range(0, image.shape[0], M):
+            for y in range(0, image.shape[1], N):
+                list.append([image[x:x + M, y:y + N]])
         list_mean = []
         for i in range(0, len(list)):
             mean = np.mean(list[i])
             list_mean.append(mean)
         array_mean = np.asarray(list_mean)
+        #convert 1D array to 2D array
         twod_array_mean = pc.oneD_array_to_twoD_array(array_mean)
         list_of_arrays.append(twod_array_mean)
     return list_of_arrays
