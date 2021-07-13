9a696467ba3f4a15a57b7bd8e6d1bb969ab6c240
RiaTimm <82507151+RiaTimm@users.noreply.github.com>
2021-07-13 15:18:42 +0200

    unittest dice

--- Test/test_dicescore.py
+++ Test/test_dicescore.py
@@ -1,13 +1,13 @@
 import unittest
 import numpy as np
 from matplotlib.image import imread
+from matplotlib.pyplot import imshow
 
 from SVM_Segmentation import dicescore
 
-
 class TestDiceScore(unittest.TestCase):
     def setUp(self) -> None:
-        self.img = np.asarray(imread('gt29test.png'))
+        self.img = np.asarray(imread('../Data/synthetic_images/synthetic_masks/mask0.png'))
 
     def test_dice_score(self):
         result = dicescore.dice_score(self.img, self.img)
