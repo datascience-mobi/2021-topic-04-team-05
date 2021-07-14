67fa4e49dfe58d8cd547e1ca210dbe1872ffa39a
laurasanchis <lasanpla@gmail.com>
2021-07-15 00:11:46 +0200

    SVM debugged!

--- SVM_Segmentation/svm_test.py
+++ SVM_Segmentation/svm_test.py
@@ -1,75 +1,88 @@
 import numpy as np
 from glob import glob
+import skimage.feature
 from tqdm import tqdm
 from skimage import io
 from skimage.transform import resize
 import matplotlib.pyplot as plt
-plt.rcParams["figure.figsize"] = (10,5)
+plt.rcParams["figure.figsize"] = (10, 5)
 import os
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split as tts
 from sklearn.metrics import accuracy_score, f1_score
-workdir = os.path.normpath("/Users/laurasanchis/PycharmProjects/2021-topic-04-team-05/")
+from skimage.feature import multiscale_basic_features, canny
+from sklearn.model_selection import StratifiedKFold
+#workdir = os.path.normpath("/Users/laurasanchis/PycharmProjects/2021-topic-04-team-05/")
 IMGSIZE = 200
 
 
 def compute_cost(W, X, Y):
     # calculate hinge loss
-    N = X.shape[0]
-    distances = 1 - Y * (np.dot(X, W))
-    distances[distances < 0] = 0  # equivalent to max(0, distance)
-    hinge_loss = regularization_strength * (np.sum(distances) / N)
-
+    N = X.shape[0] #number of pixels = number of data points
+    distances = 1 - Y * (np.dot(X, W)) #dot product=classification of pixel, multiplied with Y can be positiv (when
+    # th prediction and gt is equal) or negative ()
+    distances = np.maximum(0, distances)  # equivalent to max(0, distance)
+    hinge_loss = regularization_strength * (np.sum(distances) / N) #durch N -> normalisierung, * reg strength,
+    # wie wichtig die richtige klassifizierung ist
     # calculate cost
-    cost = 1 / 2 * np.dot(W, W) + hinge_loss
+    cost = 1 / 2 * np.dot(W, W) + hinge_loss #W^2 damit es nicht zu groß wird, hinge loss summiert damit die cost
+    # größer wird (und penalization)
     return cost
 
 def calculate_cost_gradient(W, X_batch, Y_batch):
     # if only one example is passed (eg. in case of SGD)
     if type(Y_batch) == np.float64:
         Y_batch = np.array([Y_batch])
-        X_batch = np.array([X_batch])  # gives multidimensional array
+        X_batch = np.array([X_batch])  # gives multidimensional array, 1 pixel con todas las features que tiene como
+        # columnas
 
-    distance = 1 - (Y_batch * np.dot(X_batch, W))
-    dw = np.zeros(len(W))
+    distance = 1 - (Y_batch * np.dot(X_batch, W)) #clasifica el pixel
+    dw = np.zeros(len(W)) #crea vector de weights vacío para ir llenándolo
 
     for ind, d in enumerate(distance):
-        if max(0, d) == 0:
+        if max(0, d) == 0: #si d es negativo -> pixel clasificado bien, se mantiene el weight calculado
             di = W
         else:
-            di = W - (regularization_strength * Y_batch[ind] * X_batch[ind])
-        dw += di
+            di = W - (regularization_strength * Y_batch[ind] * X_batch[ind]) #cuando pixel clasificado mal,
+            # multiplicas c por la derivada de distance (y*np.dot(x,w)), para ver cuánto quieres ir en la dirección
+            # de la derivada para corregir la clasificación
+        dw += di #derivada w
 
     dw = dw/len(Y_batch)  # average
     return dw
 
 def sgd(features, outputs):
-    max_epochs = 5000
+    max_epochs = 100
     weights = np.zeros(features.shape[1])
-    weights = np.random.random(features.shape[1])
-    nth = 0
     prev_cost = float("inf")
-    cost_threshold = 0.005  # Lower -> Longer training and better results
+    #cost_threshold = 0.01  # Lower -> Longer training and better results
     history_cost = []
     # stochastic gradient descent
-    for epoch in range(1, max_epochs):
+    patience = 0
+    for epoch in range(0, max_epochs):
         # shuffle to prevent repeating update cycles
         X, Y = shuffle(features, outputs)
         for ind, x in enumerate(X):
             ascent = calculate_cost_gradient(weights, x, Y[ind])
-            weights = weights - (learning_rate * ascent)
+            weights = weights - (learning_rate * ascent) #minimizar weights por el valor de learning rate en
+            # dirección contraria al gradiente, para encontrar el minimo
 
-        cost = compute_cost(weights, features, outputs)
+        cost = compute_cost(weights, features, outputs) #calcula el coste para comparar la epoca anterior con la
+        # actual y ver si esta avanzando
         history_cost.append(cost)
-        # convergence check on 2^nth epoch
-        if epoch == 2 ** nth or epoch == max_epochs - 1:
+        if epoch % 20 == 0 or epoch == max_epochs - 1:
             #cost = compute_cost(weights, features, outputs)
             print("Epoch is: {} and Cost is: {}".format(epoch, cost))
             # stoppage criterion
-            if abs(prev_cost - cost) < cost_threshold * prev_cost:
-                return weights, history_cost
+            if (prev_cost < cost):
+                #best_cost = min(history_cost)
+                if patience == 10:
+                    return weights, history_cost
+                else:
+                    patience += 1
+            else:
+                patience = 0
             prev_cost = cost
-            nth += 1
 
     return weights, history_cost
 
@@ -77,9 +90,11 @@ def sgd(features, outputs):
 def processImage(image_path, imgSize):
     img = io.imread(image_path)
     img = resize(img, (imgSize, imgSize))
+    img_canny = canny(img)
+    img_canny = img_canny.reshape(-1, 1)
     img = img.reshape(-1, 1)
     bias_term = np.ones(img.shape[0]).reshape(-1, 1)
-    return np.hstack([img, bias_term])
+    return np.hstack([img, img_canny, bias_term])
 
 def processMask(image_path, imgSize):
     img = io.imread(image_path)
@@ -89,19 +104,23 @@ def processMask(image_path, imgSize):
     img = img.flatten()
     return img
 
-def predict(imageIndex):
+def predict(imageIndex, W):
     data = processImage(imgs[imageIndex], IMGSIZE)
     prediction = [np.sign(np.dot(data[pixelN], W)) for pixelN in range(data.shape[0])]
     groundTruth = processMask(masks[imageIndex], IMGSIZE)
     return prediction, groundTruth
+
+def predictScore(data, gt, W):
+    pred = [np.sign(np.dot(data[pixelN], W)) for pixelN in range(data.shape[0])]
+    return f1_score(pred, gt)
 
 def pred2Image(prediction):
     prediction = np.array(prediction)
     return prediction.reshape((IMGSIZE, IMGSIZE))
 
 
-imgs = glob(workdir + "/Data/N2DH-GOWT1/img/*.tif")
-masks = glob(workdir + "/Data/N2DH-GOWT1/gt/tif/*.tif")
+imgs = sorted(glob("../Data/N2DH-GOWT1/img/*.tif"))
+masks = sorted(glob("../Data/N2DH-GOWT1/gt/tif/*.tif"))
 print(f"{len(imgs)} images detected and {len(masks)} masks detected")
 
 NImagesTraining = 4
@@ -110,22 +129,44 @@ y_train = np.concatenate([processMask(imgPath, IMGSIZE) for imgPath in masks[:NI
 X_test = [processImage(imgPath, IMGSIZE) for imgPath in imgs[NImagesTraining:]]
 y_test = [processMask(imgPath, IMGSIZE) for imgPath in masks[:NImagesTraining]]
 
+skf = StratifiedKFold(n_splits=5)
+
 regularization_strength = 10000
-learning_rate = 0.0000001
-W, hist = sgd(X_train, y_train)
+learning_rate = 0.00001
 
+model = {}
+for splitnumber, (train_index, test_index) in enumerate(skf.split(X_train, y_train)):
+    X_train_split, X_test_split = X_train[train_index], X_train[test_index]
+    y_train_split, y_test_split = y_train[train_index], y_train[test_index]
+    w, hist = sgd(X_train_split, y_train_split)
+    f1 = predictScore(X_test_split, y_test_split, w)
+    model[splitnumber] = {"train_index": train_index, "test_index": test_index, "w": w, "hist": hist, "f1": f1,
+                          "image_size": IMGSIZE}
+    print(model[splitnumber]["w"])
 
-fig = plt.plot(hist)
-_ = plt.ylabel("Cost function")
-_ = plt.xlabel("Epoch")
-_ = plt.title(f"Final Weights {W}")
+f1_mean_model = np.mean([model[i]["f1"] for i in model.keys()])
+w_mean_model = np.mean([model[i]["w"] for i in model.keys()], axis=0)
+
+output_dir = '../Data/N2DH-GOWT1/pred'
+
+for i in range(len(model.keys())):
+    fig = plt.plot(model[i]['hist'], label=f"Split {i}")
+    _ = plt.ylabel("Cost function")
+    _ = plt.xlabel("Epoch")
+plt.legend()
+plt.savefig(f"{output_dir}/lr-{learning_rate}-reg-{regularization_strength}.png")
+
+img_names = []
+for filename in sorted(os.listdir('../Data/N2DH-GOWT1/img')):
+        img_names.append(filename)
 
 Ntest = len(imgs) - NImagesTraining
-fig, ax = plt.subplots(ncols=Ntest, dpi=110)
-fig.tight_layout(w_pad=-2)
+fig, ax = plt.subplots(dpi=90)
 for i in range(Ntest):
     ii = i+NImagesTraining
-    pred, gt = predict(ii)
-    ax[i].imshow(pred2Image(pred), cmap='cool')
-    ax[i].axis('Off')
-    ax[i].set_title(f"Test img: {ii+1} F1:{round(f1_score(gt, pred), 2)}")
+    pred, gt = predict(ii, w_mean_model)
+    ax.imshow(pred2Image(pred), cmap='gray')
+    ax.axis('On')
+    ax.set_title(f"Test img: {ii+1} F1:{round(f1_score(gt, pred), 2)}")
+    plt.savefig(f"{output_dir}/{img_names[ii]}_pred_lr-{learning_rate}-reg-{regularization_strength}.png")
+
