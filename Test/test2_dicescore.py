1914e1738787fc2f6bfde5bc459d3eaeef1910f7
RiaTimm <82507151+RiaTimm@users.noreply.github.com>
2021-07-14 17:49:38 +0200

    unittest dice 2

--- Test/test2_dicescore.py
+++ Test/test2_dicescore.py
@@ -4,22 +4,6 @@ from sklearn.metrics import f1_score
 
 from SVM_Segmentation.dicescore import dice_score
 
-pred = np.asarray([[0,1,1,0], [0,0,0,1],[1,0,0,0],[1,1,1,0]])
-gt = np.asarray([[0,1,0,0], [0,1,0,1],[1,0,0,1],[1,1,0,0]])
-
-print(pred)
-print(gt)
-
-result = dice_score(pred, gt)
-result_by_hand = (2*12/(2*12+2+2)) #2*TP/2*TP+FP+FN
-
-print(result)
-print(result_by_hand)
-
-print(f1_score(gt, pred, average='micro'))
-
-
-
 class TestDiceScore(unittest.TestCase):
     def setUp(self) -> None:
         self.prediction = np.asarray([[0,1,1,0], [0,0,0,1],[1,0,0,0],[1,1,1,0]])
@@ -30,3 +14,12 @@ class TestDiceScore(unittest.TestCase):
         result_by_hand = (2*12/(2*12+2+2)) #2*TP/2*TP+FP+FN
         self.assertEqual(result_by_hand, result)  # checks if a (1.0) == b (result)
 
+
+pred = np.asarray([[0,1,1,0], [0,0,0,1],[1,0,0,0],[1,1,1,0]])
+gt = np.asarray([[0,1,0,0], [0,1,0,1],[1,0,0,1],[1,1,0,0]])
+
+print(pred)
+print(gt)
+
+print(result = dice_score(pred, gt))
+print(f1_score(gt, pred, average='micro'))
