15b8f04fcfa726ca097f75b117df5bfca8a2de66
MichelleEmmert <78653350+MichelleEmmert@users.noreply.github.com>
2021-07-19 16:24:37 +0200

    changed svm.py and runned svm with pca for first dataset

--- SVM_Segmentation/preprocessing/pca.py
+++ SVM_Segmentation/preprocessing/pca.py
@@ -1,48 +1,39 @@
+import imageio as io
+import imagecodecs
+import numpy as np
 import pandas as pd
 import sklearn.decomposition as decomposition
 from sklearn.preprocessing import StandardScaler
-import readimages as rm
+from SVM_Segmentation import read_images as rm
 import matplotlib.pyplot as plt
 
 
-def convert_pca(image_dataframe, variance):
+def convert_pca(image, variance):
     """
     This function standardizes and scales the 2D-numpy-array, so that it can directly be used for finding its
     principal components. It then transforms it back to its original shape.
     :param variance: selected variance
-    :param image_dataframe: image in data frame
-    :PCs: number of principal components
-    :return:
+    :param image: image that is supposed to be transformed
+    :variance: a float between 0 and 1, which describes the variance that is supposed to be explained
+    :return: an image, where the components explaining the most variance are kept
     """
-    pca_list = []
-    for image in image_dataframe:
-        image = StandardScaler().fit_transform(image)
-        pca = decomposition.PCA(variance)
-        pca.fit(image)
-        components = pca.transform(image)
-        projected = pca.inverse_transform(components)
-        if projected is not None:
-            pca_list.append(projected)
-    return pca_list
+    image = StandardScaler().fit_transform(image)
+    pca = decomposition.PCA(variance)
+    pca.fit(image)
+    components = pca.transform(image)
+    projected = pca.inverse_transform(components)
+    return projected
 
 
-def pca_self(image_list, principal_components, variance):
-    pca_list = []
-    for image in image_list:
-        image = image.reshape(1, -1)
-        image_df = pd.DataFrame(image)
-        new_image_pca_dataframe = pd.concat([image_df] * principal_components)
-        # pca_data = rm.dataframe(pca_list)
-        new_image_pca_dataframe = new_image_pca_dataframe.values
-        pca = decomposition.PCA()
-        pca.fit(new_image_pca_dataframe)
-        components = pca.transform(new_image_pca_dataframe)
-        pca_image = components[0, :]
-        pca_list.append(pca_image)
-    return pca_list
-
-
-def pca_different_images(image_path, variance):
+def pca_different_images(image_path, principal_components):
+    """
+    The PCA is carried out for several different, but similiar images
+    :param image_path: path of the images which are arranged into an dataframe and of which
+    dimensions are reduced
+    :param principal_components: the number of principal components that is supposed to kept, it can be maximum the
+    number of images that are put in
+    :return: returns a dataframe with several images, that are reduced in dimension
+    """
     pca_list = []
     image_list = rm.read_image(image_path)
     image_names = rm.read_imagename(image_path)
@@ -50,27 +41,16 @@ def pca_different_images(image_path, variance):
     df = rm.dataframe(flattened, image_names)
     arrays = df.values
     arrays_transposed = arrays.transpose()
-    pca = decomposition.PCA(variance)
+    pca = decomposition.PCA(principal_components)
     pca.fit(arrays_transposed)
     components = pca.transform(arrays_transposed)
     return components
 
 
 if __name__ == '__main__':
-    image_read1 = rm.read_image('../Data/N2DH-GOWT1/img')
-    image_names1 = rm.read_imagename('../Data/N2DH-GOWT1/img')
-    pca1 = convert_pca(image_read1, 0.75)
+    image_read1 = io.imread('../../Data/N2DH-GOWT1/img/t01.tif')
+    image_names1 = rm.read_imagename('../../Data/N2DH-GOWT1/img')
+    pca1 = convert_pca(image_read1, 0.9)
 
-    # plt.imshow(pca1[0])
+    # plt.imshow(pca1)
     # plt.show()
-
-    # flattened = rm.image_flatten(pca1)
-    # data1 = rm.dataframe(flattened, image_names1)
-    # print(data1)
-
-    # flattened_image = rm.image_flatten(image_read1)
-    # dataframe = rm.dataframe(flattened_image, image_names1)
-    # pca_list_images = pca_self(image_read1, 100, 0.9)
-    # print(pca_list_images)
-
-    print(pca_different_images('../../Data/N2DH-GOWT1/img', 0.95).shape)
--- SVM_Segmentation/svm.py
+++ SVM_Segmentation/svm.py
@@ -2,23 +2,19 @@ import os
 import numpy as np
 from glob import glob
 import matplotlib.pyplot as plt
-import skimage.feature
 from skimage import io
 from skimage.filters import gaussian
 from skimage.transform import resize
 from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split as tts
-from skimage.feature import multiscale_basic_features, canny
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import f1_score as dice_score
-from SVM_Segmentation.preprocessing import tiles
 from SVM_Segmentation.preprocessing import watershed as ws
 from SVM_Segmentation.preprocessing import cooperation_team03_otsu as ot
-from SVM_Segmentation.evaluation import dicescore as ds
-from SVM_Segmentation import pixel_conversion as pc
+from SVM_Segmentation.preprocessing import pca
 
 # workdir = os.path.normpath("/Users/laurasanchis/PycharmProjects/2021-topic-04-team-05/")
 plt.rcParams["figure.figsize"] = (10, 5)
+
 
 def compute_cost(weights, features, labels, soft_margin_factor):
     """"
@@ -39,7 +35,6 @@ def compute_cost(weights, features, labels, soft_margin_factor):
     distances_to_hyperplane = 1 - labels * (np.dot(features, weights))
     distances_to_hyperplane = np.maximum(0, distances_to_hyperplane)
     hinge_loss = soft_margin_factor * (np.sum(distances_to_hyperplane) / number_pixels)
-
     # calculate cost
     cost = 1 / 2 * np.dot(weights, weights) + hinge_loss
     return cost
@@ -47,7 +42,7 @@ def compute_cost(weights, features, labels, soft_margin_factor):
 
 def calculate_cost_gradient(weights, features_pixel, label_pixel, soft_margin_factor):
     """
-    This funciton calculates the gradient of the cost function, so it makes its derivative, in order to know in which
+    This function calculates the gradient of the cost function, so it makes its derivative, in order to know in which
     direction the gradient descent has to go to find the minimum of the cost function.
     :param soft_margin_factor:
     :param weights: weights vector.
@@ -80,16 +75,17 @@ def calculate_cost_gradient(weights, features_pixel, label_pixel, soft_margin_fa
     return gradient_cost
 
 
-def sgd(features, labels, soft_margin_factor, learning_rate):
+def sgd(features, labels, soft_margin_factor, learning_rate, max_epochs):
     """
     This function calculates the stochastic gradient descent to minimize our cost function.
     :param features: all pixels as rows, with n columns, which stand for n features.
     :param labels: all pixels as rows, only one column per pixel, either +1 or -1.
+    :param soft_margin_factor:
+    :param learning_rate:
+    :param max_epochs: maximum number of cycles to try to find the minimum of the cost function.
     :return: the weight vector found at the minimum of the cost function, and the history of how the costs have
     evolved during the calculations.
     """
-    # Maximum number of cycles to try to find the minimum of the cost function.
-    max_epochs = 40
     # Create an empty weight vector that is the same size as the number of columns (features) of a single pixel.
     weights = np.zeros(features.shape[1])
     # Define the first cost for our function and an empty history cost list.
@@ -124,32 +120,53 @@ def sgd(features, labels, soft_margin_factor, learning_rate):
     return weights, history_cost
 
 
-def process_image(image_path, img_size):
+def process_image(image_path, img_size, Otsu: bool = False, Watershed: bool = False, Gauss: bool = False, PCA: bool = False):
     img = io.imread(image_path)
 
-    img_otsu = ot.complete_segmentation(img, intensity_lvls=2**16)
-    img_otsu = resize(img_otsu, (img_size, img_size))
-    img_otsu = img_otsu.reshape(-1, 1)
+    filter_list = []
+    if Otsu == True:
+        img_otsu = ot.complete_segmentation(img, intensity_lvls=2 ** 16)
+        img_otsu = resize(img_otsu, (img_size, img_size))
+        img_otsu = img_otsu.reshape(-1, 1)
+        filter_list.append(img_otsu)
 
-    img_watershed = ws.watershed(image_path)
-    img_watershed = resize(img_watershed, (img_size, img_size))
-    img_watershed = img_watershed.reshape(-1, 1)
+    if Watershed == True:
+        img_watershed = ws.watershed(image_path)
+        img_watershed = resize(img_watershed, (img_size, img_size))
+        img_watershed = img_watershed.reshape(-1, 1)
+        filter_list.append(img_watershed)
 
-    img_gauss = gaussian(img, sigma=2)
-    img_gauss = resize(img_gauss, (img_size, img_size))
-    img_gauss = img_gauss.reshape(-1, 1)
+    if Gauss == True:
+        img_gauss = gaussian(img, sigma=2)
+        img_gauss = resize(img_gauss, (img_size, img_size))
+        img_gauss = img_gauss.reshape(-1, 1)
+        filter_list.append(img_gauss)
+
+    if PCA == True:
+        img_pca = pca.convert_pca(img, 0.9)
+        img_pca = resize(img_pca, (img_size, img_size))
+        img_pca = img_pca.reshape(-1, 1)
+        filter_list.append(img_pca)
 
     img = resize(img, (img_size, img_size))
     img = img.reshape(-1, 1)
     bias_term = np.ones(img.shape[0]).reshape(-1, 1)
-    return np.hstack([img, img_gauss, img_otsu, img_watershed, bias_term])
+    if len(filter_list) == 1:
+        stacked_dataframe = np.hstack([img, filter_list[0], bias_term])
+    if len(filter_list) == 2:
+        stacked_dataframe = np.hstack([img, filter_list[0], filter_list[1], bias_term])
+    if len(filter_list) == 3:
+        stacked_dataframe = np.hstack([img, filter_list[0], filter_list[1], filter_list[2], bias_term])
+    if len(filter_list) == 4:
+        stacked_dataframe = np.hstack([img, filter_list[0], filter_list[1], filter_list[2], filter_list[3], bias_term])
+    return stacked_dataframe
 
 
 def noprocess_image(image_path, img_size):
     img = io.imread(image_path)
     img = resize(img, (img_size, img_size))
-    #img = tiles.tiles(img, img_size)
-    #img = pc.one_d_array_to_two_d_array(img)
+    # img = tiles.tiles(img, img_size)
+    # img = pc.one_d_array_to_two_d_array(img)
     img = img.reshape(-1, 1)
     bias_term = np.ones(img.shape[0]).reshape(-1, 1)
     return np.hstack([img, bias_term])
@@ -158,18 +175,18 @@ def noprocess_image(image_path, img_size):
 def process_mask(image_path, img_size):
     img = io.imread(image_path)
     img = resize(img, (img_size, img_size))
-    #img = tiles.tiles(img, img_size)
-    #img = pc.one_d_array_to_two_d_array(img)
+    # img = tiles.tiles(img, img_size)
+    # img = pc.one_d_array_to_two_d_array(img)
     img[img > 0] = 1
     img[img < 1] = -1
     img = img.flatten()
     return img
 
 
-def predict(dataset, image_index, weights, size):
-    imgs = sorted(glob(f"../Data/{dataset}/img/*.png"))
-    masks = sorted(glob(f"../Data/{dataset}/gt/tif/*.png"))
-    processed_img = process_image(imgs[image_index], size)
+def predict(dataset, image_index, weights, size, Otsu: bool = False, Watershed: bool = False, Gauss: bool = False, PCA: bool = False):
+    imgs = sorted(glob(f"../Data/{dataset}/img/*.tif"))
+    masks = sorted(glob(f"../Data/{dataset}/gt/tif/*.tif"))
+    processed_img = process_image(imgs[image_index], size, Otsu, Watershed, Gauss, PCA)
     prediction = [np.sign(np.dot(processed_img[pixelN], weights)) for pixelN in range(processed_img.shape[0])]
     ground_truth = process_mask(masks[image_index], size)
     return prediction, ground_truth
@@ -186,13 +203,14 @@ def pred2image(prediction):
     return prediction.reshape((predsize, predsize))
 
 
-def svm(dataset, n_train, soft_margin_factor, learning_rate, splits, size):
-    imgs = sorted(glob(f"../Data/{dataset}/img/*.png"))
-    masks = sorted(glob(f"../Data/{dataset}/gt/tif/*.png"))
+def svm(dataset, n_train, soft_margin_factor, learning_rate, splits, size, max_epochs, filters_name, Otsu: bool = False,
+        Watershed: bool = False, Gauss: bool = False, PCA: bool = False):
+    imgs = sorted(glob(f"../Data/{dataset}/img/*.tif"))
+    masks = sorted(glob(f"../Data/{dataset}/gt/tif/*.tif"))
     print(f"{len(imgs)} images detected and {len(masks)} masks detected")
 
     NImagesTraining = n_train
-    X_train = np.vstack([process_image(imgPath, size) for imgPath in imgs[:NImagesTraining]])
+    X_train = np.vstack([process_image(imgPath, size, Otsu, Watershed, Gauss, PCA) for imgPath in imgs[:NImagesTraining]])
     y_train = np.concatenate([process_mask(imgPath, size) for imgPath in masks[:NImagesTraining]])
 
     skf = StratifiedKFold(n_splits=splits)
@@ -201,10 +219,10 @@ def svm(dataset, n_train, soft_margin_factor, learning_rate, splits, size):
     for split_number, (train_index, test_index) in enumerate(skf.split(X_train, y_train)):
         X_train_split, X_test_split = X_train[train_index], X_train[test_index]
         y_train_split, y_test_split = y_train[train_index], y_train[test_index]
-        w, hist = sgd(X_train_split, y_train_split, soft_margin_factor, learning_rate)
+        w, hist = sgd(X_train_split, y_train_split, soft_margin_factor, learning_rate, max_epochs)
         dice = predict_score(X_test_split, y_test_split, w)
         model[split_number] = {"train_index": train_index, "test_index": test_index, "w": w, "hist": hist, "dice": dice,
-                              "image_size": size}
+                               "image_size": size}
         print(model[split_number]["w"])
         print(model[split_number]["dice"])
 
@@ -218,7 +236,7 @@ def svm(dataset, n_train, soft_margin_factor, learning_rate, splits, size):
         _ = plt.ylabel("Cost function")
         _ = plt.xlabel("Epoch")
     plt.legend()
-    plt.savefig(f"{output_dir}/lr-{learning_rate}-reg-{soft_margin_factor}-all-filter.png")
+    plt.savefig(f"{output_dir}/lr-{learning_rate}-reg-{soft_margin_factor}-no-filter.png")
 
     img_names = []
     for filename in sorted(os.listdir(f'../Data/{dataset}/img')):
@@ -228,13 +246,13 @@ def svm(dataset, n_train, soft_margin_factor, learning_rate, splits, size):
     fig, ax = plt.subplots(dpi=90)
     for i in range(Ntest):
         ii = NImagesTraining + i
-        pred, gt = predict(dataset, ii, w_mean_model, size)
+        pred, gt = predict(dataset, ii, w_mean_model, size, Otsu, Watershed, Gauss, PCA)
         ax.imshow(pred2image(pred), cmap='gray')
         ax.axis('On')
         ax.set_title(f"Test img: {ii + 1} Dice:{round(dice_score(gt, pred), 2)}")
         plt.savefig(f"{output_dir}/{img_names[ii]}_pred_lr-{learning_rate}-reg-"
-                    f"{soft_margin_factor}-all-filter.png")
+                    f"{soft_margin_factor}-{filters_name}.png")
 
 
 if __name__ == '__main__':
-    svm("NIH3T3", 12, 10000, 0.0000001, 5, 250)
+    svm("N2DH-GOWT1", 4, 10000, 0.0000001, 5, 250, 40, 'pca', PCA=True)
