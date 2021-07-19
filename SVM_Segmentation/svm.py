b4b033faac4142fba73e8cac884d16561af52a28
RiaTimm <82507151+RiaTimm@users.noreply.github.com>
2021-07-19 20:30:59 +0200

    report

--- SVM_Segmentation/svm.py
+++ SVM_Segmentation/svm.py
@@ -353,5 +353,5 @@ def synthetic_svm(dataset, synth_dataset, soft_margin_factor, learning_rate, spl
 
 
 if __name__ == '__main__':
-    synthetic_svm("N2DL-HeLa", "N2DL-HeLa_t13", 10000, 0.0000001, 3, 250, 40, "Otsu", Otsu=True, Watershed=False,
+    synthetic_svm("N2DL-HeLa", "N2DL-HeLa_t13", 10000, 0.0000001, 3, 250, 40, "Watershed", Otsu=False, Watershed=True,
     Gauss= False, PCA=False)
